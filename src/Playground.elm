e5edd62161054303d44f85a40b8bfe70aa2c5551
Oscar Ortiz <ortizg.oscar@gmail.com>
2021-02-07 17:20:52 -0300

    change new let scope

--- src/Playground.elm
+++ src/Playground.elm
@@ -2,13 +2,18 @@ module Playground exposing (main)
 
 import Html
 
-escapeEarth : Float -> Float -> String
-escapeEarth myVelocity mySpeed =
+escapeEarth : Float -> Float -> String -> String
+escapeEarth myVelocity mySpeed fuelStatus =
   let
     escapeVelocityInKmPerSecond =
       11.186
     orbitalSpeedInKmPerSecond =
       7.67
+    whereToLand fuel =
+      if fuel == "low" then
+        "Land on the droenship"
+      else
+        "Land on the launchpad"
   in
 
   if myVelocity > escapeVelocityInKmPerSecond then
@@ -16,7 +21,7 @@ escapeEarth myVelocity mySpeed =
   else if mySpeed == orbitalSpeedInKmPerSecond then
     "Stay in orbit"
   else
-    "Come back"
+    whereToLand fuelStatus
 
 computedSpeed : Float -> Float -> Float
 computedSpeed distance time =
@@ -31,12 +36,15 @@ computedTime startTime endTime =
 
 main : Html.Html msg
 main =
-  -- Html.text (escapeEarth 11 (computedSpeed 7.67 (computedTime 2 3)))
--- TODO  Difference between "|>" operatro and ">>" operator :  https://www.reddit.com/r/elm/comments/67izmq/when_to_use_vs/
-  computedTime 2 3
-    |> computedSpeed 7.67
-    |> escapeEarth 11
-    |> Html.text
+--   -- Html.text (escapeEarth 11 (computedSpeed 7.67 (computedTime 2 3)))
+-- -- TODO  Difference between "|>" operatro and ">>" operator :  https://www.reddit.com/r/elm/comments/67izmq/when_to_use_vs/
+--   computedTime 2 3
+--     |> computedSpeed 7.67
+--     |> escapeEarth 11 
+--     |> "low"
+--     |> Html.text
+      escapeEarth 10 6.7 "low"
+        |> Html.text
 
   
 
